ba7c372716a615220b4f44c8b567b5a8cb0c0c02
Kapil <talktokapildev@gmail.com>
2018-06-16 14:08:19 +0100

    Home Screen: Avoiding keyboard and Header

--- app/screens/Home.js
+++ app/screens/Home.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
-import { View, StatusBar } from "react-native";
+import { View, StatusBar, KeyboardAvoidingView } from "react-native";
 
 import { Container } from "../components/Container";
 import { Logo } from "../components/Logo"; 
 import { InputWithButton } from "../components/TextInput";
 import { ClearButton } from "../components/Buttons";
 import { LastConverted } from "../components/Text";
+import { Header } from "../components/Header";
 
 const TEMP_BASE_CURRENCY = 'USD';
 const TEMP_QUOTE_CURRENCY = 'GBP';
@@ -32,10 +33,16 @@ class Home extends Component{
         console.log('press swap currency');
     }
 
+    handleOptionsPress = () => {
+        console.log('options press');
+      };
+
     render() {
         return (
           <Container>
             <StatusBar backgroundColor="blue" barStyle="light-content" />
+            <Header onPress={this.handleOptionsPress}/>
+            <KeyboardAvoidingView behavior="padding">
             <Logo />
             <InputWithButton buttonText={TEMP_BASE_CURRENCY} onPress={this.handlePressBaseCurrency}
             defaultValue={TEMP_BASE_PRICE}
@@ -53,6 +60,7 @@ class Home extends Component{
             />
             <ClearButton text="Reverse Currencies"
             onPress={this.handleSwapCurrency}/>
+            </KeyboardAvoidingView>
           </Container>
         );
       }
