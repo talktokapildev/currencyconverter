b181c80cab2d0f2dfe84be35c13f3036a55736c1
Kapil <talktokapildev@gmail.com>
2018-06-16 13:35:26 +0100

    Home Screen: Styled Text

--- app/screens/Home.js
+++ app/screens/Home.js
@@ -5,11 +5,14 @@ import { Container } from "../components/Container";
 import { Logo } from "../components/Logo"; 
 import { InputWithButton } from "../components/TextInput";
 import { ClearButton } from "../components/Buttons";
+import { LastConverted } from "../components/Text";
 
 const TEMP_BASE_CURRENCY = 'USD';
 const TEMP_QUOTE_CURRENCY = 'GBP';
 const TEMP_BASE_PRICE = '100';
 const TEMP_QUOTE_PRICE = '79.74';
+const TEMP_CONVERSION_RATE = 0.7974;
+const TEMP_CONVERSION_DATE = new Date();
 
 class Home extends Component{
 
@@ -42,6 +45,12 @@ class Home extends Component{
             <InputWithButton buttonText={TEMP_QUOTE_CURRENCY} onPress={this.handlePressQuoteCurrency} 
             editable={false}
             value={TEMP_QUOTE_PRICE}></InputWithButton>
+            <LastConverted
+              base={TEMP_BASE_CURRENCY}
+             quote={TEMP_QUOTE_CURRENCY}
+             conversionRate={TEMP_CONVERSION_RATE}
+             date={TEMP_CONVERSION_DATE}
+            />
             <ClearButton text="Reverse Currencies"
             onPress={this.handleSwapCurrency}/>
           </Container>
