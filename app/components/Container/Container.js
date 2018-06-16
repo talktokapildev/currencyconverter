f0a7059ae4c6051397e5150ccd52342c013213a2
Kapil <talktokapildev@gmail.com>
2018-06-16 12:28:25 +0100

    Home Screen: Button

--- app/components/Container/Container.js
+++ app/components/Container/Container.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {View} from 'react-native';
+import { View, TouchableWithoutFeedback, Keyboard } from 'react-native';
 import styles from "./styles";
 
+
 const Container = ({children}) => (
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
     <View style={styles.container}>
-    {children}
+      {children}
     </View>
+  </TouchableWithoutFeedback>
 )
 
 Container.propTypes = {
