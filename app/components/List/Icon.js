3860cf9501fc2d949c0321bcb355fd357d1284e7
Kapil <talktokapildev@gmail.com>
2018-06-17 13:27:03 +0100

    Themes

--- app/components/List/Icon.js
+++ app/components/List/Icon.js
@@ -5,13 +5,15 @@ import { View, Image } from 'react-native';
 
 import styles from './styles';
 
-const Icon = ({checkmark, visible}) => {
+const Icon = ({checkmark, visible, iconBackground}) => {
     if (visible) {
         const iconStyles = [styles.icon];
         if (visible) {
           iconStyles.push(styles.iconVisible);
         }
-    
+        if(iconBackground){
+            iconStyles.push({backgroundColor: iconBackground});
+        }
         return (
           <View style={iconStyles}>
             {checkmark
@@ -31,6 +33,7 @@ const Icon = ({checkmark, visible}) => {
 Icon.propTypes = {
     visible: PropTypes.bool,
     checkmark: PropTypes.bool,
+    iconBackground: PropTypes.string
   };
 
 export default Icon;
--- app/index.js
+++ app/index.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import EStyleSheet from "react-native-extended-stylesheet";
-import Options from "./screens/Options";
+import Themes from "./screens/Themes";
 
 EStyleSheet.build({
     $primaryBlue: '#4F6D7A',
-
+    $primaryOrange: '#D57A66',
+    $primaryGreen: '#00BD9D',
+    $primaryPurple: '#9E768F',
+    
   $white: '#FFFFFF',
   $lightGray: '#F0F0F0',
   $border: '#E2E2E2',
   $inputText: '#797979', 
   $darkText: '#343434',
 })
-export default () => <Options/>;
+export default () => <Themes/>;
 
 // export default class App extends React.Component {
 //   render() {
