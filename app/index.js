2e6a91d403d610b22cb65b9ee64102de6690e369
Kapil <talktokapildev@gmail.com>
2018-06-17 12:00:28 +0100

    Home Screen: ListItem

--- app/index.js
+++ app/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import EStyleSheet from "react-native-extended-stylesheet";
-import Home from "./screens/Home";
+import CurrencyList from "./screens/CurrencyList";
 
 EStyleSheet.build({
     $primaryBlue: '#4F6D7A',
 
   $white: '#FFFFFF',
   $lightGray: '#F0F0F0',
-  $border: '#979797',
-  $inputText: '#797979',
+  $border: '#E2E2E2',
+  $inputText: '#797979', 
+  $darkText: '#343434',
 })
-export default () => <Home/>;
+export default () => <CurrencyList/>;
 
 // export default class App extends React.Component {
 //   render() {
